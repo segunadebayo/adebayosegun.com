3296b7fc6864f47c01c2121e4ce3cfde578071a2
Segun Adebayo <joseshegs@gmail.com>
2021-12-31 12:15:48 +0100

    feat: add arrow right icon

--- components/social-icons.tsx
+++ components/social-icons.tsx
@@ -89,3 +89,16 @@ export function WebsiteIcon(props: IconProps) {
     </svg>
   );
 }
+
+export function ArrowRightIcon(props: IconProps) {
+  return (
+    <svg width="23" height="19" viewBox="0 0 23 19" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M14.0607 1.43932L13 0.378662L10.8787 2.49998L11.9393 3.56064L16.3787 7.99998H0V11H16.3787L11.9393 15.4393L10.8787 16.5L13 18.6213L14.0607 17.5606L21.0607 10.5606L22.1213 9.49998L21.0607 8.43932L14.0607 1.43932Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
