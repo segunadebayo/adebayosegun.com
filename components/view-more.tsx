d8fa74112b5f8d1b162d64dd0e2077e1847d14a7
Segun Adebayo <joseshegs@gmail.com>
2021-12-31 12:16:06 +0100

    refactor: component to use arrow right icon

--- components/view-more.tsx
+++ components/view-more.tsx
@@ -1,29 +1,16 @@
-import { HStack, Text, useToken } from '@chakra-ui/react';
-import Link from 'next/link';
-
-function ArrowRightIcon() {
-  const sageBase = useToken('colors', 'sage.base');
-  return (
-    <svg width="23" height="19" viewBox="0 0 23 19">
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M14.0607 1.43932L13 0.378662L10.8787 2.49998L11.9393 3.56064L16.3787 7.99998H0V11H16.3787L11.9393 15.4393L10.8787 16.5L13 18.6213L14.0607 17.5606L21.0607 10.5606L22.1213 9.49998L21.0607 8.43932L14.0607 1.43932Z"
-        fill={sageBase}
-      />
-    </svg>
-  );
-}
+import { Box, HStack, Text, useToken } from '@chakra-ui/react';
+import { ArrowRightIcon } from './social-icons';
 
 export default function ViewMore({ children, href = '#' }) {
+  const sageBase = useToken('colors', 'sage.base');
   return (
-    <Link href={href} passHref>
-      <HStack as="a" display="inline-flex">
-        <Text fontWeight={'bold'} color="sage.base">
-          {children}
-        </Text>
-        <ArrowRightIcon />
-      </HStack>
-    </Link>
+    <HStack as="a" display="inline-flex" data-group>
+      <Text fontWeight="bold" color="sage.base">
+        {children}
+      </Text>
+      <Box transform="auto" transition="transform 0.2s" _groupHover={{ translateX: '3px' }}>
+        <ArrowRightIcon color={sageBase} />
+      </Box>
+    </HStack>
   );
 }
