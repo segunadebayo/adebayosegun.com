9ea0c21a7956aa977daef87b4b337780390629e1
Segun Adebayo <joseshegs@gmail.com>
2022-01-01 23:01:51 +0100

    feat: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,10 @@ export default function Document() {
         <meta charSet="utf-8" />
         <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
         <link href="/static/favicons/site.webmanifest" rel="manifest" />
+        <link href="/static/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+        <link href="/static/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+        <link href="/static/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+        <meta content="#FEB48C" name="theme-color" />
         {['PolySans-Regular', 'PolySans-Medium'].map((font) => (
           <link
             key={font}
