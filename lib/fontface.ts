ac18ea79d04540c40756715b6d2e700af8b0ce34
Segun Adebayo <joseshegs@gmail.com>
2022-01-05 14:43:42 +0100

    chore: add inter font locally for better loading perf

--- lib/fontface.ts
+++ lib/fontface.ts
@@ -5,14 +5,22 @@ const fontFace = css`
     font-family: 'PolySans';
     src: url(/fonts/PolySans-Regular.woff2) format('woff2');
     font-weight: 500;
-    font-display: swap;
+    font-display: optional;
   }
 
   @font-face {
     font-family: 'PolySans';
     src: url(/fonts/PolySans-Medium.woff2) format('woff2');
     font-weight: 700;
-    font-display: swap;
+    font-display: optional;
+  }
+
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 100 900;
+    font-display: optional;
+    src: url(/fonts/Inter.woff2) format('woff2');
   }
 `;
 
