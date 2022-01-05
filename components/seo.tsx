8d1cf7ae04a3a17b5340984468bdae40cf4167e6
Segun Adebayo <joseshegs@gmail.com>
2022-01-05 23:46:15 +0100

    feat: add support for custom og:image

--- components/seo.tsx
+++ components/seo.tsx
@@ -5,6 +5,7 @@ import siteConfig from 'site.config';
 type SEOProps = {
   title?: string;
   description?: string;
+  image?: string;
   post?: {
     date?: string;
     tags?: string[];
@@ -12,12 +13,13 @@ type SEOProps = {
 };
 
 export default function SEO(props: SEOProps) {
-  const { title, description, post } = props;
+  const { title, description, post, image } = props;
   return (
     <NextSeo
       title={title}
       description={description}
       openGraph={{
+        images: image ? [{ url: image }] : undefined,
         title,
         description,
         article: post ? { publishedTime: post.date, tags: post.tags } : undefined,
