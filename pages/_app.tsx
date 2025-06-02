e5caa777e6919f76d381b7b8375da698e46db82f
Segun Adebayo <joseshegs@gmail.com>
2025-06-02 17:09:22 +0100

    chore: add speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import fontFace from 'lib/fontface';
 import theme from 'lib/theme';
 import { DefaultSeo } from 'next-seo';
@@ -10,6 +11,7 @@ import '../styles/prism.css';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <SpeedInsights />
       <DefaultSeo {...seo} />
       <ChakraProvider theme={theme}>
         <Global styles={fontFace} />
