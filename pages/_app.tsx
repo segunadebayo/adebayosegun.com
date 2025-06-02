06904a0bca2978a7372a45b59571aced0a7208ba
Segun Adebayo <joseshegs@gmail.com>
2025-06-02 17:07:56 +0100

    chore: add speed insight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import { Global } from '@emotion/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Provider } from 'components/ui/provider';
 import fontFace from 'lib/fontface';
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import seo from 'site.config';
 import '../styles/prism.css';
-import { Provider } from 'components/ui/provider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <SpeedInsights />
       <DefaultSeo {...seo} />
       <Global styles={fontFace} />
       <Provider>
